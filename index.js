9790fcfc99b99867414a024178fe3f18fb8991e5
Odychrist <odychristdahounliton@gmail.com>
2026-05-07 22:31:55 +0100

    Nouvelle modification concernant la fonctionnalité WhatsApp

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const buttons = document.querySelectorAll(".btn-cmd");
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", () => {
+    let message = "";
+    let encodedMessage = "";
     const productDiv = btn.closest(".product-div");
-    const text = productDiv.querySelector("h3").textContent;
 
-    const message = `Bonjour, j'aimerais commander : ${text}`;
-    const encodedMessage = encodeURIComponent(message);
-
+    if (productDiv) {
+      const text = productDiv.querySelector("h3").textContent;
+      message = `Bonjour, j'aimerais commander : ${text}`;
+      encodedMessage = encodeURIComponent(message);
+    } else {
+      message = `Bonjour, j'aimerais commander un de vos produits.`;
+      encodedMessage = encodeURIComponent(message);
+    }
+    // console.log(message, encodedMessage);
     const phone = "2290152118061";
 
     window.open(`https://wa.me/${phone}?text=${encodedMessage}`, "_blank");
